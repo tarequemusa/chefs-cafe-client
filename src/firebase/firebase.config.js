e7b82740c6afd9681bd1d204cb4135195a7f26bf
tarequemusa <tareque.web@gmail.com>
2023-05-04 10:20:48 +0600

    Hide Firebase Configuration SDK

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,14 @@ import {initializeApp} from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment variable ', import.meta.env.VITE_PASS);
 const firebaseConfig = {
-    apiKey: "AIzaSyAPFTLlt7jbnPlNLqPZMp-il-09aRXAyoc",
-    authDomain: "the-chefs-cafe.firebaseapp.com",
-    projectId: "the-chefs-cafe",
-    storageBucket: "the-chefs-cafe.appspot.com",
-    messagingSenderId: "267168512466",
-    appId: "1:267168512466:web:8ae91679765dc25c26b00b"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
