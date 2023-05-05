9e4f438e0a6dc61293aa7b3aaf56d648a7031e8b
tarequemusa <tareque.web@gmail.com>
2023-05-05 19:17:00 +0600

    Minor updated

--- src/pages/Home/RecipeCard/RecipeCard.jsx
+++ src/pages/Home/RecipeCard/RecipeCard.jsx
@@ -1,57 +1,64 @@
 import React, {useState} from 'react';
-import {Button, Card, Col, Row, Toast} from 'react-bootstrap';
-import {FaBreadSlice, FaCalendar, FaHeart} from 'react-icons/fa';
+import {Button, Card, Col, Row, Table, Toast} from 'react-bootstrap';
+import {FaBreadSlice, FaCalendar, FaHeart, FaRegStar, FaStar} from 'react-icons/fa';
+import {Rating} from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const RecipeCard = ({recipe}) => {
-    const {picture, name, short_bio, recipe_name, likes, ingredients, cooking_method, years_of_experience, num_recipes} = recipe;
+    const {picture, name, short_bio, recipe_name, likes, ingredients, cooking_method, years_of_experience, rating, num_recipes} = recipe;
     // <img src={recipe.banner} alt="" />
     const [show, setShow] = useState(false);
+
     return (
         <>
-            <Row>
-                <Card className="text-center mb-4">
+            <>
+                <Card className='my-2'>
                     <Card.Header>
                         <h4><span className='d-flex justify-content-start text-success fw-bold'>{name}</span></h4>
-                    </Card.Header>
-                    <Card.Body>
-                        <Row className='d-flex mx-auto justify-content-center' xs={1} md={2}>
-                            <Col>
-                                <img className='w-50 mb-3 mx-auto d-flex justify-content-between ml-0' src={picture} alt="" />
-                            </Col>
-                            <Col xs={8}>
-                                <h4 className='fw-bold border-bottom mb-3 mx-0 text-start text-success'>The Chefs Cafe: Short Bio</h4>
-                                <p>{short_bio}</p>
-                            </Col>
+                        <Row xs={1}>
+                            <div className='d-flex'>
+                                <p className='fw-bold px-4 rounded py-1'><small><span className='text-danger'><FaHeart /></span>{' '}Likes: {' '}{likes}</small></p>
+                                <p className='fw-bold mx-2 px-4 rounded py-1'><small><span className='text-danger'><FaCalendar /></span>{' '}Years of Experience: {' '} {years_of_experience}</small></p>
+                                <p className='fw-bold px-4 rounded py-1'><small><span className='text-danger'><FaBreadSlice /></span>{' '}Numbers of Recipes: {' '}{num_recipes}</small></p>
+                            </div>
                         </Row>
-                    </Card.Body>
-                    <Row>
-                        <Card.Footer className="text-muted">
-                            <Button variant="outline-dark" className='fw-bold px-4 rounded py-1'><small><FaHeart />{' '}Likes: {' '}{likes}</small></Button>
-                            <Button variant="outline-dark" className='fw-bold mx-2 px-4 rounded my-1 py-1'><small><span className='text-danger'><FaCalendar /></span>{' '}Years of Experience: {' '} {years_of_experience}</small></Button>
-                            <Button variant="outline-dark" className='fw-bold px-4 rounded py-1'><small><span className='text-danger'><FaBreadSlice /></span>{' '}Numbers of Recipes: {' '}{num_recipes}</small></Button>
-                        </Card.Footer>
+
+                    </Card.Header>
+                    <Row className='d-flex mx-auto justify-content-center' xs={1} md={2}>
+                        <Col>
+                            <img className='w-50 mb-3 mx-auto d-flex justify-content-between ml-0' src={picture} alt="" />
+                        </Col>
+                        <Col xs={8}>
+                            <p className='mt-3'>{short_bio}</p>
+                        </Col>
                     </Row>
                 </Card>
-            </Row>
+            </>
             <Row>
-                <Card className='p-4 mb-4'>
-                    <Card.Body>
+                <Card className="text-center mb-4">
+                    <Card.Footer className="text-muted">
                         <h4 className='fw-bold border-bottom mb-3 mx-0 text-start text-success'>The Chefs Cafe: Recipe Details</h4>
-                        <p>{short_bio}</p>
-                        <Row>
-                            <Card.Title><span className='fw-bold text-success'>Recipe Name:</span>{' '}{recipe_name}</Card.Title>
-                            <Card.Text>
-                                <span className='fw-bold text-success'>Cooking Method:</span> {' '}{[cooking_method]}
-                            </Card.Text>
-                            <span className='fw-bold text-success'>Ingredients:</span> {' '} {ingredients}
-                        </Row>
-                    </Card.Body>
-                    <Card.Footer>
+                        <Card.Title><span className='fw-bold text-success'>Recipe Name:</span>{' '}{recipe_name}</Card.Title>
+                        <Card.Text>
+                            <span className='fw-bold text-success'>Cooking Method:</span> {' '}{[cooking_method]}
+                        </Card.Text>
+                        <span className='fw-bold text-success'>Ingredients:</span> {' '} {ingredients}
                         <Row className='d-flex flex-column justify-content-center text-center'>
-                            <Col>
-                                <Button onClick={() => setShow(true)}><small><FaHeart />{' '}Favourite: {' '}{likes}</small></Button>
+
+                            <Col className='d-flex mt-4 justify-content-between'>
+                                <div className='w-25 d-flex align-items-center'>
+                                    <Rating>
+                                        placeholderRating={rating}
+                                        readonly
+                                        emptySymbol={<FaRegStar />}
+                                        placeholderSymbol={<FaStar className='text-warning' />}
+                                        fullSymbol={<FaStar />}
+                                    </Rating>
+                                    <span className='fw-bold mt-2'>{rating}</span>
+                                </div>
+                                <Button onClick={() => setShow(true)}><small><FaHeart />{' '}Favourite {' '}</small></Button>
                             </Col>
-                            <Col className='mt-3'>
+                            <Col className='mt-3 text-center'>
                                 <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide>
                                     <Toast.Header>
                                         <div className='text-danger display-6'>
@@ -66,7 +73,40 @@ const RecipeCard = ({recipe}) => {
                     </Card.Footer>
                 </Card>
             </Row>
+            {/* <Row>
+                <Table responsive>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            {Array.from({length: 12}).map((_, index) => (
+                                <th key={index}>Table heading</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            {Array.from({length: 12}).map((_, index) => (
+                                <td key={index}>Table cell {index}</td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            {Array.from({length: 12}).map((_, index) => (
+                                <td key={index}>Table cell {index}</td>
+                            ))}
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            {Array.from({length: 12}).map((_, index) => (
+                                <td key={index}>Table cell {index}</td>
+                            ))}
+                        </tr>
+                    </tbody>
+                </Table>
+            </Row> */}
         </>
+
     );
 };
 
--- src/pages/OpenHour/OpenHour.jsx
+++ src/pages/OpenHour/OpenHour.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Col, Row} from 'react-bootstrap';
-import {FaAssistiveListeningSystems, FaBreadSlice, FaCookie, FaFish, FaPastafarianism, FaPoop} from 'react-icons/fa';
+import {FaAssistiveListeningSystems, FaBreadSlice, FaCookie, FaEgg, FaFish, FaPastafarianism, FaPoop} from 'react-icons/fa';
 import ReactPlayer from 'react-player';
 
 const OpenHour = () => {
@@ -12,35 +12,46 @@ const OpenHour = () => {
                 <h3 className='fw-bold text-center mb-5'>DELICIOUS RECIPE</h3>
             </div>
             <Col sm>
-                <div className='bg-primary px-5 border border-success shadow'>
-                    <div className='text-center text-light' style={{fontSize: '4rem'}}>
+                <div className='shadow'>
+                    <div className='bg-danger text-center text-light py-3' style={{fontSize: '4rem'}}>
                         <FaFish></FaFish>
                     </div>
-                    <h3 className='text-center text-light p-4 border rounded-circle fw-bold'>Fish</h3>
-                </div>
-            </Col>
-            <Col sm><div className='bg-success px-5 border border-success shadow'>
-                <div className='text-center text-light' style={{fontSize: '4rem'}}>
-                    <FaCookie></FaCookie>
-                </div>
-                <h3 className='text-center text-light p-4 border rounded-circle fw-bold'>Sneaks</h3>
-            </div></Col>
-            <Col sm>
-                <div className='bg-danger px-5 border border-success shadow'>
-                    <div className='text-center text-light' style={{fontSize: '4rem'}}>
-                        <FaPastafarianism></FaPastafarianism>
-                    </div>
-                    <h3 className='text-center text-light p-4 border rounded-circle fw-bold'>Meat</h3>
+                    <h3 className='text-center text-dark fw-bold py-3'>Fish</h3>
                 </div>
             </Col>
             <Col sm>
-                <div className='bg-info px-5 border border-success shadow'>
-                    <div className='text-center text-light' style={{fontSize: '4rem'}}>
+                <div className='shadow'>
+                    <div className='bg-info text-center text-light py-3' style={{fontSize: '4rem'}}>
                         <FaAssistiveListeningSystems></FaAssistiveListeningSystems>
                     </div>
-                    <h3 className='text-center text-light p-4 border rounded-circle fw-bold'>Pastas</h3>
+                    <h3 className='text-center text-dark fw-bold py-3'>Pasta</h3>
                 </div>
             </Col>
+            <Col sm>
+                <div className='shadow'>
+                    <div className='bg-success text-center text-light py-3' style={{fontSize: '4rem'}}>
+                        <FaPastafarianism></FaPastafarianism>
+                    </div>
+                    <h3 className='text-center text-dark fw-bold py-3'>Meat</h3>
+                </div>
+            </Col>
+            <Col sm>
+                <div className='shadow'>
+                    <div className='bg-primary text-center text-light py-3' style={{fontSize: '4rem'}}>
+                        <FaEgg></FaEgg>
+                    </div>
+                    <h3 className='text-center text-dark fw-bold py-3'>Sneaks</h3>
+                </div>
+            </Col>
+            <Col sm>
+                <div className='shadow'>
+                    <div className='bg-secondary text-center text-light py-3' style={{fontSize: '4rem'}}>
+                        <FaCookie></FaCookie>
+                    </div>
+                    <h3 className='text-center text-dark fw-bold py-3'>Vegetable</h3>
+                </div>
+            </Col>
+
         </Row>
 
 
